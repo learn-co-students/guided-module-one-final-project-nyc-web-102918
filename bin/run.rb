168c82ac939c9f9185cb7f883a9eb3eb76522327
hsterman <heather.sterman531@gmail.com>
2018-11-13 16:17:20 -0500

    stuff

--- bin/run.rb
+++ bin/run.rb
@@ -1,2 +1,16 @@
-require_relative 'config/environment'
+require_relative '../config/environment'
+# require_relative "../lib/character_api.rb"
 
+def type_character_name
+  puts "Type in character name"
+  #character_name = gets.chomp
+end
+
+def type_in_name
+  character_name = gets.chomp
+end
+
+
+type_character_name
+name = type_in_name
+get_character_from_api(name)
---
+++ lib/character_api.rb
@@ -0,0 +1,20 @@
+require 'rest-client'
+require 'json'
+require 'pry'
+
+
+
+def get_character_from_api(character_name)
+  #make web request
+  get_character = RestClient.get('https://rickandmortyapi.com/api/character/')
+  character_hash = JSON.parse(get_character)
+    character_hash["results"].each do |info|
+      if info["name"] == character_name
+      end
+
+binding.pry
+0
+  end
+end
+binding.pry
+0
